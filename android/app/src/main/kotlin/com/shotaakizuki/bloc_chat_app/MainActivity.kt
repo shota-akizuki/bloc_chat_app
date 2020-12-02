c5dc39cdd8dba60aea12712d999a69ea498c6665
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2020-12-03 02:31:25 +0900

    1st Commit

---
+++ android/app/src/main/kotlin/com/shotaakizuki/bloc_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shotaakizuki.bloc_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_chat_app/screens/chat_screen.dart';
+import 'package:bloc_chat_app/screens/login_screen.dart';
+import 'package:bloc_chat_app/screens/registration_screen.dart';
+import 'package:bloc_chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(ChatApp());
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: <String, WidgetBuilder>{
+        WelcomeScreen.id: (BuildContext context) => WelcomeScreen(),
+        LoginScreen.id: (BuildContext context) => LoginScreen(),
+        RegistrationScreen.id: (BuildContext context) => RegistrationScreen(),
+        ChatScreen.id: (BuildContext context) => ChatScreen(),
+      },
+    );
+  }
+}
