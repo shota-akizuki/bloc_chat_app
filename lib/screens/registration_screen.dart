c7db92b0a16e3d79cfd60f621f4853141cd448c8
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2020-12-04 03:15:48 +0900

    7th Commit ・Cloud FireStoreデータ保存を実装

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
-
 import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
